4d4147309d7dc98dc4e167d4f45f2ce9221bc6ea
Timo Turkki <timo.turkki@frantic.com>
2020-05-20 18:03:36 +0300

    add test to check username exists when creting user

--- part4/blog-list-app-backend/tests/blog_api.test.js
+++ part4/blog-list-app-backend/tests/blog_api.test.js
@@ -290,6 +290,26 @@ describe('when there is one user at db', () => {
       expect(usersAtEnd).toHaveLength(usersAtStart.length);
     });
 
+    it('should fail with proper statuscode and message if username is missing', async () => {
+      const usersAtStart = await helper.usersInDb();
+
+      const newUser = {
+        name: 'Root Vegetable',
+        password: 'myster',
+      };
+
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/);
+
+      expect(result.body.error).toContain('`username` is required.');
+
+      const usersAtEnd = await helper.usersInDb();
+      expect(usersAtEnd).toHaveLength(usersAtStart.length);
+    });
+
     it('should fail with proper statuscode and message if password is too short', async () => {
       const usersAtStart = await helper.usersInDb();
 
