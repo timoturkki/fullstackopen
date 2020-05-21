36c958a9ab3330f411c6aafd70dd708f9b865d98
Timo Turkki <timo.turkki@frantic.com>
2020-05-21 18:35:47 +0300

    fix error with missing hook dependency

--- part5/bloglist-frontend/src/components/Notification.js
+++ part5/bloglist-frontend/src/components/Notification.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 
 const Notification = ({ notification, deleteHandler }) => {
   const { msg, isAlert } = notification;
+  const deleteCallback = () => deleteHandler();
+  const memoizedDeleteHandler = useCallback(deleteCallback, []);
 
   useEffect(() => {
-    const timer = setTimeout(deleteHandler, 6000);
+    const timer = setTimeout(memoizedDeleteHandler, 6000);
     return () => clearTimeout(timer);
-  }, []);
+  }, [memoizedDeleteHandler]);
 
   return <p className={`notification${isAlert ? ' error' : ''}`}>{msg}</p>;
 };
