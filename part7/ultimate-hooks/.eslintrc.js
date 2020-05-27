93fd4e0925ea5f67c5ffbe664cf1580fe114324a
Timo Turkki <timo.turkki@frantic.com>
2020-05-27 18:28:26 +0300

    add ultimate-hooks app

---
+++ part7/ultimate-hooks/.eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [ 
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2,
+            { "SwitchCase": 1 }
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-case-declarations": "off",
+        "eqeqeq": "error",
+        "no-trailing-spaces": "error",
+        "comma-dangle": ['error', 'always-multiline'],
+        "object-curly-spacing": [
+            "error", "always"
+        ],
+        "arrow-spacing": [
+            "error", { "before": true, "after": true }
+        ],
+        "no-console": 0,
+        "react/prop-types": 0
+    }
+}
