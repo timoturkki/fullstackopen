51de86d7872908ff1cde8e5108b733bcbfeaf10f
Timo Turkki <timo.turkki@frantic.com>
2020-05-27 18:49:01 +0300

    complete useResource hook

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -1,57 +1,61 @@
-  
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 const useField = (type) => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState('');
 
   const onChange = (event) => {
-    setValue(event.target.value)
-  }
+    setValue(event.target.value);
+  };
 
   return {
     type,
     value,
-    onChange
-  }
-}
+    onChange,
+  };
+};
 
 const useResource = (baseUrl) => {
-  const [resources, setResources] = useState([])
+  const [resources, setResources] = useState([]);
 
-  // ...
+  useEffect(() => {
+    const fetchResources = async () => {
+      const response = await axios.get(baseUrl);
+      setResources(response.data);
+    };
 
-  const create = (resource) => {
-    // ...
-  }
+    fetchResources();
+  }, [baseUrl]);
 
-  const service = {
-    create
-  }
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource);
+    setResources([...resources, response.data]);
+  };
 
-  return [
-    resources, service
-  ]
-}
+  const service = { create };
+
+  return [ resources, service ];
+};
 
 const App = () => {
-  const content = useField('text')
-  const name = useField('text')
-  const number = useField('text')
+  const content = useField('text');
+  const name = useField('text');
+  const number = useField('text');
 
-  const [notes, noteService] = useResource('http://localhost:3005/notes')
-  const [persons, personService] = useResource('http://localhost:3005/persons')
+  const [notes, noteService] = useResource('http://localhost:3005/notes');
+  const [persons, personService] = useResource('http://localhost:3005/persons');
 
   const handleNoteSubmit = (event) => {
-    event.preventDefault()
-    noteService.create({ content: content.value })
-  }
- 
+    event.preventDefault();
+    noteService.create({ content: content.value });
+  };
+
   const handlePersonSubmit = (event) => {
-    event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
-  }
+    event.preventDefault();
+    personService.create({ name: name.value, number: number.value });
+  };
 
   return (
     <div>
@@ -70,7 +74,7 @@ const App = () => {
       </form>
       {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
